7d0c8669054a092da0dd2f454cb1c9ed559ad6f4
Mark deGroat <mhdegroat@gmail.com>
2017-03-07 18:14:04 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array) # put argument(s) here
+  # code here
+  i = 0
+  return_array = []
+  while i < array.length
+    return_array.push(yield array[i])
+    i += 1
+  end
+  return_array
+end
